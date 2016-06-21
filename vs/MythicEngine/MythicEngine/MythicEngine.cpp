661e121f302609427a34c0d31a7f582ec2318d5e
Kenan Millet <KVMillet@Gmail.com>
2016-06-21 17:07:09 -0400

    Initial Creation of the project

---
+++ vs/MythicEngine/MythicEngine/MythicEngine.cpp
@@ -0,0 +1,20 @@
+// MythicEngine.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <cstdlib>
+
+
+int main()
+{
+	try
+	{
+		
+	}
+	catch(...)
+	{
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
+
